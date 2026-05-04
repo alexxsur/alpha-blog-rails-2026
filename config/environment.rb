ecc5af4ea159a5277cbec73e3643f433f989b518
Alex <alexxsur@gmail.com>
2026-05-03 20:45:33 -0400

    Style validations and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Este bloque anula el comportamiento por defecto de Rails de envolver
+# los campos con errores en un <div class="field_with_errors">.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  # Devolvemos el tag HTML original marcado como seguro para el navegador.
+  html_tag.html_safe
+end
