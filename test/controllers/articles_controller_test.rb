f90428a7873f462d9d147d33b9a0d9bacdd202c5
Alex <alexxsur@gmail.com>
2026-06-13 11:58:31 -0400

    Update articles unit tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -11,40 +11,48 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should show article" do
+    get article_url(@article)
+    assert_response :success
+  end
+
+  # --- A partir de aquí todas las acciones requieren inicio de sesión ---
+
   test "should get new" do
+    sign_in_as(@user) # Iniciamos sesión antes de pedir el formulario
     get new_article_url
     assert_response :success
   end
 
   test "should create article" do
+    sign_in_as(@user) # Iniciamos sesión antes de hacer el POST
     assert_difference("Article.count") do
-      post articles_url, params: { article: {
-        description: @article.description,
-        title: @article.title,
-        user_id: @user.id
-      }
+      post articles_url, params: {
+        article: {
+          description: @article.description,
+          title: @article.title
+          # El controlador debería asignarlo usando el current_user de la sesión.
+        }
       }
     end
 
     assert_redirected_to article_url(Article.last)
   end
 
-  test "should show article" do
-    get article_url(@article)
-    assert_response :success
-  end
-
-  test "should get edit" do
+  test "test_should_get_edit" do
+    sign_in_as(@user) # Iniciamos sesión antes de pedir la edición
     get edit_article_url(@article)
     assert_response :success
   end
 
   test "should update article" do
+    sign_in_as(@user) # Iniciamos sesión antes de enviar la actualización
     patch article_url(@article), params: { article: { description: @article.description, title: @article.title } }
     assert_redirected_to article_url(@article)
   end
 
   test "should destroy article" do
+    sign_in_as(@user) # Iniciamos sesión antes de borrar
     assert_difference("Article.count", -1) do
       delete article_url(@article)
     end
