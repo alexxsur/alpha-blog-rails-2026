75a12ca7d749146bb30d1c67991581dc4aec304d
Alex <alexxsur@gmail.com>
2026-06-14 20:42:31 -0400

    Fix flash alert not displaying on failed login
    Add `status: :unprocessable_entity` to the sessions controller when
    authentication fails.
    
    In Rails 7+, Turbo requires a 422 error status to properly re-render
    the forms and display `flash.now` messages when a form submission fails.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       flash.now[:alert] = "There was something wrong with your login details"
-      render "new"
+      render "new", status: :unprocessable_entity
     end
   end
 
